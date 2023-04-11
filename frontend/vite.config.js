5df35027cfff2bacd2ec1f0bafa8438a713613ff
Shhhamsy <daur2001@mail.ru>
2023-04-11 04:09:53 +0300

    new test2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['axios', 'react-chat-engine-pretty']
-  }
 })
 
 // https://vitejs.dev/config/

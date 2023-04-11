15c914ae5c788ba5661156fe9d3889ff2d797445
Shhhamsy <daur2001@mail.ru>
2023-04-11 04:22:12 +0300

    new test3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
-import "./App.css";
-
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
+import AuthPage from "./authPage.jsx";
+import ChatsPage from "./chatsPage.jsx";
 
 function App() {
-    const [user, setUser] = useState(undefined);
+    const [user, setUser] = useState();
 
     if (!user) {
         return <AuthPage onAuth={(user) => setUser(user)} />;
